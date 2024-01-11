79a491c540bbd2180cde6b51f775d45dcd68d737
Akshay_M <105048001+akshaym1412@users.noreply.github.com>
2024-01-11 10:55:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,9 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors());
+app.use(cors({origin:["http://localhost:5173"],
+             methods:["GET","POST","PUT","DELETE"],
+             credentials:true}));
 app.get("/",(req,res)=>{
     res.json("Hello World");
 })
