d661dee0d28bcf1af1cccbfa846771f3bc82baa2
Akshay_M <harshaakshay14@gmail.com>
2024-01-11 09:34:41 +0530

    3nd commit

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:["https://blog-app1-beta.vercel.app"],
+app.use(cors({origin:["https://blog-app1-beta.vercel.app/"],
              methods:["GET","POST","PUT","DELETE"],
              credentials:true}))
 app.get("/",(req,res)=>{
