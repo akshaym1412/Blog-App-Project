68ab5c506b2fe87e7afc8f5edf8a8b06aa9d0e32
Akshay_M <105048001+akshaym1412@users.noreply.github.com>
2024-01-18 12:57:22 +0530

    Update verifyToken.js

--- backend/verifyToken.js
+++ backend/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken')
 
 const verifyToken=async(req,res,next)=>{
-    const token=req.cookies.access_token;
+    const {token}=req.cookies;
     // console.log(token)
     if(!token){
         return res.status(401).json("You are not authenticated!")
