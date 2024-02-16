bec5e7f34486bf11f590ddd3fc68b7b180f1817b
Akshay_M <105048001+akshaym1412@users.noreply.github.com>
2024-02-16 09:30:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors({origin:[process.env.FRONTEND_URL],
-             methods:["GET","POST","PUT","DELETE"],
+             methods:["GET","POST","PUT","DELETE","PATCH"],
              credentials:true}));
 app.get("/",(req,res)=>{
     res.json("Hello World");
